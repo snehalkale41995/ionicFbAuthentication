dafb50eca6b3a43491f8ba42cf7573d4e5cf9e17
snehalkale41995 <kalesnehal1007@gmail.com>
2020-05-10 13:51:12 +0530

    added sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'about-us', loadChildren: './about-us/about-us.module#AboutUsPageModule' },
+  { path: 'contact-us', loadChildren: './contact-us/contact-us.module#ContactUsPageModule' },
+
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Pages } from './interfaces/pages';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  public appPages: Array<Pages>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    
   ) {
+    this.appPages = [
+      {
+        title: 'Home',
+        url: '/home',
+        direct: 'root',
+        icon: 'home'
+      },
+      {
+        title: 'About',
+        url: '/about-us',
+        direct: 'forward',
+        icon: 'information-circle-outline'
+      },
+
+      {
+        title: 'App Settings',
+        url: '/contact-us',
+        direct: 'forward',
+        icon: 'cog'
+      }
+    ];
     this.initializeApp();
   }
 
